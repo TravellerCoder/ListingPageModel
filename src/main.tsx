6fb7c74d6054ced2cfe327f3348ed6401190cf76
Traveller Coder <guillermobukowski@gmail.com>
2025-09-06 10:23:59 -0300

    instalacion y config de router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router-dom'
+import { LayoutMain } from './components/layouts/LayoutMain.tsx'
+import ProductDetail from './pages/productDetail/ProductDetail.tsx'
+import Home from './pages/home/Home.tsx'
 import './index.css'
 import App from './App.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LayoutMain />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/detail",
+        element: <ProductDetail />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
