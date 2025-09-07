6967d76f0dfc0ee38878b69d0c24f71ca6be23d1
Traveller Coder <guillermobukowski@gmail.com>
2025-09-07 12:18:45 -0300

    maquetado inicial de Hero

---
+++ src/components/ui/hero/Hero.tsx
@@ -0,0 +1,12 @@
+
+export const Hero = () => {
+  return (
+    <div className="">
+        <div className="">
+            <h1>T.C Listing Page</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <button>Get Started</button>
+        </div>
+    </div>
+  )
+}
