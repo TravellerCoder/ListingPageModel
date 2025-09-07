eb826d747e8ede04f26f570b47624748ad074db1
Traveller Coder <guillermobukowski@gmail.com>
2025-09-07 12:55:31 -0300

    estilado basico de hero

--- src/components/layouts/LayoutMain.tsx
+++ src/components/layouts/LayoutMain.tsx
@@ -1,10 +1,12 @@
 import  { NavBar } from "../ui/navbar/NavBar"
 import { Outlet } from "react-router-dom"
+import { Hero } from "../ui/hero/Hero"
 
 export const LayoutMain = () => {
     return (
         <div>
             <NavBar /> 
+            <Hero />
             <Outlet />
         </div>
     )
--- src/components/ui/hero/Hero.tsx
+++ src/components/ui/hero/Hero.tsx
@@ -1,12 +1,13 @@
+import styles from './hero.module.css';
 
 export const Hero = () => {
   return (
-    <div className="">
-        <div className="">
-            <h1>T.C Listing Page</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            <button>Get Started</button>
+    <div className={styles.heroContainer}> 
+            <div className={styles.institutional}>
+                <h1>T.C Listing Page</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <button>Get Started</button>
+            </div>
         </div>
-    </div>
   )
 }
