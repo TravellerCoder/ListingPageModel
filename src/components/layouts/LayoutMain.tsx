ab62a7efaf764d724ce82c31a6e395ca7d2205d2
Traveller Coder <guillermobukowski@gmail.com>
2025-09-07 14:23:04 -0300

    los productos llegan al front

--- src/components/layouts/LayoutMain.tsx
+++ src/components/layouts/LayoutMain.tsx
@@ -1,12 +1,11 @@
 import  { NavBar } from "../ui/navbar/NavBar"
 import { Outlet } from "react-router-dom"
-import { Hero } from "../ui/hero/Hero"
+
 
 export const LayoutMain = () => {
     return (
         <div>
             <NavBar /> 
-            <Hero />
             <Outlet />
         </div>
     )
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,31 @@
+import { Hero } from "../../components/ui/hero/Hero"
+import { useState, useEffect } from "react"
+
 const Home = () => {
+
+    /* Fetch productos desde la api */
+    const [products, setProducts] = useState([]);
+
+    const getProducts = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/products');
+            const data = await response.json();
+            setProducts(data);
+        } catch (error) {
+            console.error('Error fetching products:', error);
+        }
+    }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
+
+    
     return (
-        <div>Home</div>
+        <div>
+            <Hero/>
+        </div>
     )
 }
 
-export default Home
+export default Home 
