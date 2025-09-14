223a859268ef790ae36b4b0e361c4f80ca498684
Traveller Coder <guillermobukowski@gmail.com>
2025-09-14 12:07:42 -0300

    modificacion de textos de hero

--- src/components/ui/hero/Hero.tsx
+++ src/components/ui/hero/Hero.tsx
@@ -5,9 +5,9 @@ export const Hero = () => {
     <div className={styles.heroContainer}> 
             <img src="/src/assets/Casa-moderna-un-piso.jpg" className={styles.heroBg} alt="Hero" />
             <div className={styles.heroTexting}>
-                <h1>T.C Listing Page</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                <button>Get Started</button>
+                <h1>T.C Broker Inmobiliario</h1>
+                <p>Experiencia y confianza para tu próxima operación inmobiliaria.</p>
+                <button>Contactate</button>
             </div>
         </div>
   )
