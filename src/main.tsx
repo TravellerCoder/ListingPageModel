bfe2ffbc2537a0c0cdf4a8b96e087cc9ec9f1c8e
Traveller Coder <guillermobukowski@gmail.com>
2025-10-13 14:18:56 -0300

    reestructuracion de carpetas

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { LayoutMain } from './components/layouts/LayoutMain.tsx'
-import ProductDetail from './pages/productDetail/ProductDetail.tsx'
+import ProductCart from './pages/productCart/ProductCart.tsx'
 import Home from './pages/home/Home.tsx'
 import AboutUs from './pages/aboutUs/AboutUs.tsx'
 import Contact from './pages/contact/Contact.tsx'
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/products",
-        element: <ProductDetail />,
+        element: <ProductCart />,
       },
       {
         path: "/about",
