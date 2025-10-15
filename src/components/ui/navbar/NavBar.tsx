32b0fd40ec2203001faa10625bcd9f5f42c6ab58
Traveller Coder <guillermobukowski@gmail.com>
2025-10-15 11:29:37 -0300

    Link para logo navbar

--- src/components/ui/navbar/NavBar.tsx
+++ src/components/ui/navbar/NavBar.tsx
@@ -1,15 +1,17 @@
 import styles from './NavBar.module.css';
+import { Link } from 'react-router-dom';
+
 
 export const NavBar = () => {
   return (
     <div className={styles.navBarContainer}>
         <div className={styles.logo}>  
-            T.C Broker Inmobiliario
+            <Link to="/">T.C Broker Inmobiliario</Link>
         </div>
         <div className={styles.navLinks}>
-            <a href="/products">Propiedades</a>
-            <a href="/about">Quienes Somos</a>
-            <a href="/contact">Contacto</a>
+            <Link to="/products">Propiedades</Link>
+            <Link to="/about">Quienes Somos</Link>
+            <Link to="/contact">Contacto</Link>
         </div>
     </div>
     )
