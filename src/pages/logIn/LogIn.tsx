5de3ecdbbbca8e5cf7cd7d4f0a60a8631345b859
Traveller Coder <guillermobukowski@gmail.com>
2025-10-19 10:19:02 -0300

    form de logIn terminado con validaciones

--- src/pages/logIn/LogIn.tsx
+++ src/pages/logIn/LogIn.tsx
@@ -1,8 +1,42 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './LogIn.module.css';
+
+
 
 const LogIn = () => {
 
+    const [email, setEmail] = useState<string >('');
+    const [password, setPassword] = useState<string>('');
+    const [error, setError] = useState<string | null>(null);
+    const navigate = useNavigate();
 
+    // Definicion de credenciales 
+
+    const validOnlyAdminMail = "admin@admin.com"; 
+    const validOnlyAdminPassword = "admin123";
+
+    // Manejo del submit del formulario
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+    //  Válidcacion de campos vacios 
+
+    if (!email || !password) {
+        setError( 'Por favor, complete todos los campos.' );
+        return;
+    }
+
+    // Validaciones de credenciales 
+
+    if ( email === validOnlyAdminMail && password === validOnlyAdminPassword) {
+        // Credenciales correctas redireccionan al admin page 
+        navigate('/admin');
+    } else {
+        // Credenciales incorrectas muestran error 
+        setError( 'Credenciales incorrectas.' );
+    }
+    };
 
   return (
     <div className={styles.logInContainer}>
@@ -10,13 +44,24 @@ const LogIn = () => {
       <form className={styles.logInForm}>
         <div className={styles.formGroup}>
           <label htmlFor="email">Correo Electrónico</label>
-          <input type="email" id="email" required />
+          <input 
+            type="email" 
+            id="email" 
+            value={email}
+            onChange={(e) => setEmail (e.target.value)} 
+            />
         </div>
         <div className={styles.formGroup}>
           <label htmlFor="password">Contraseña</label>
-          <input type="password" id="password" required />
+          <input 
+            type="password" 
+            id="password" 
+            value={password}
+            onChange={(e) => setPassword (e.target.value)}
+            />
         </div>
-        <button type="submit">Iniciar Sesión</button>
+        {error && <p className={styles.errorMessage}>{error}</p>}
+        <button type="submit" onClick={handleSubmit}>Iniciar Sesión</button>
       </form>
     </div>
   )
