be41c6c6934e18903ab4dbb7431ad4dfdb151c47
Traveller Coder <guillermobukowski@gmail.com>
2025-10-22 13:24:18 -0300

    envoltura de main con provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { LayoutMain } from './components/layouts/LayoutMain.tsx'
+import { ProductsProvider } from './context/ProductContext.tsx'
 import ProductCart from './pages/productCart/ProductCart.tsx'
 import ProductDetail from './pages/productDetail/ProductDetail.tsx'
 import Home from './pages/home/Home.tsx'
@@ -50,6 +51,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ProductsProvider>
+      <RouterProvider router={router} />
+    </ProductsProvider> 
   </StrictMode>,
 )
